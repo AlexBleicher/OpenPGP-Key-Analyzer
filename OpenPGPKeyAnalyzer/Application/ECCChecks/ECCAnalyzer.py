670b7b04c4eb90683070743d25f860f2ca526b55
bleicheral88417 <bleicheral88417@th-nuernberg.de>
2025-06-17 20:30:59 +0200

    Started working on it (will do more as soon as I got the time)

--- OpenPGPKeyAnalyzer/Application/ECCChecks/ECCAnalyzer.py
+++ OpenPGPKeyAnalyzer/Application/ECCChecks/ECCAnalyzer.py
@@ -6,4 +6,6 @@ def analyzeECCKWeaknesses(key_info, output, settings):
     foundWeaknesses.append(createWeaknessJSON("No checks for ECC implemented yet",
                                               "No checks for ECC implemented yet",
                                               "No checks for ECC implemented yet"))
+    #TODO: Check for used curves (Literature: BSI and RFC9580)
+    #TODO: Check parameters for possible side channel (need to check if feasible in practice)
     output["Found Weaknesses"] = foundWeaknesses
